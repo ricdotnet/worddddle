af297c11b9d1aeabe55901c4a62d6f7cded12979
Ricardo Rocha <ricardo.freitasrocha@cgi.com>
2022-10-17 12:08:23 +0100

    * revert store changes;

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { createApp } from 'vue';
-import { StoreManager } from '@idevelopthings/vue-class-stores/vue';
 import './style.css';
 import App from './App.vue';
 
 const app = createApp(App)
-app.use(StoreManager, import.meta.glob('./Stores/Generated/StoreLoader.ts', {eager : true}));
 
 app.mount('#app');
